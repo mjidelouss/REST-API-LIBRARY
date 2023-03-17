f1c4af0b2a91cd822ff7467a62474743f42e9e27
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2023-03-17 10:34:20 +0100

    Crud Genres...(Done)

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -21,7 +21,10 @@ class GenreController extends Controller
      */
     public function index()
     {
-
+        $genres = Genre::all();
+        return response()->json([
+            'genres' => $genres,
+        ]);
     }
 
     /**
@@ -32,40 +35,73 @@ class GenreController extends Controller
      */
     public function store(Request  $request)
     {
-        
+        $genre = Genre::create($request->all());
+        return response()->json([
+            'Message' => "Genre has been added successfully!",
+        ], 200);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Article  $article
+     * @param  \App\Models\Genre  $genre
      * @return \Illuminate\Http\Response
      */
-    public function show(Article  $article)
+    public function show($id)
     {
-        
+        $genre = Genre::find($id);
+        if(!$genre){
+            return response()->json([
+                'Message' => "Genre with #$id not Found!!"
+            ]);
+        }
+        return response()->json([
+            'Genre' => $genre->name,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Article  $article
+     * @param  \App\Models\Genre  $genre
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,Article $article)
+    public function update(Request $request, $id)
     {   
+        $genre = Genre::find($id);
 
+        if(!$genre){
+            return response()->json([
+                'Message' => "Genre with #$id not Found!!"
+            ]);
+        }
+        $genre->update($request->all());
+
+        return response()->json([
+            'Message' => "Genre has been updated Successfully!!",
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Article  $article
+     * @param  \App\Models\Genre  $genre
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Article $article)
+    public function destroy($id)
     {
-        
+        $genre = Genre::find($id);
+
+        if(!$genre){
+            return response()->json([
+                'Message' => "Genre with #$id not Found!!"
+            ]);
+        }
+        $genre->delete();
+
+        return response()->json([
+            'Message' => "Genre has been Deleted Successfully!!",
+        ], 200);
     }
 }
